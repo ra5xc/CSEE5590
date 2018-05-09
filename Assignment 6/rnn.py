9fb4f409b3682bed0ca23e664c480e59a774a4dc
ra5xc <ra5xc@mail.umkc.edu>
2018-05-08 23:50:22 -0500

    Update rnn.py

--- Assignment 6/rnn.py
+++ Assignment 6/rnn.py
@@ -1,26 +1,3 @@
-#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-"""Example of recurrent neural networks over characters for DBpedia dataset.
-
-This model is similar to one described in this paper:
-   "Character-level Convolutional Networks for Text Classification"
-   http://arxiv.org/abs/1509.01626
-
-and is somewhat alternative to the Lua code from here:
-   https://github.com/zhangxiangxiao/Crepe
-"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
